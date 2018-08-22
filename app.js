f1f1d130a1c30884c7c312095f7b9aa9eeea8681
warrenbrown <warren@katapultmedia.com>
2018-08-22 07:43:15 -0500

    added bootstrap template

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
 });
 
 app.listen(port, function() {
